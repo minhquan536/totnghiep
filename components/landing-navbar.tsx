f2fb40f00414dc59fe1d3b3d3111fb099e6d2c64
Quan <tmquan.19it4@vku.udn.vn>
2024-01-06 22:20:52 +0700

    update lại html

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -28,7 +28,7 @@ export const LandingNavbar = () => {
                 />
                 </div>
                 <h1 className={cn("text-2xl font-bold text-white", font.className)}>
-                    Genius
+                    MinhQuan
                 </h1>
             </Link>
             <div className="flex items-center gap-x-2">
